337be58d0bb1f1be9e6488eba666982bbf4167ef
Oreno <kanekosho@Kanekos-MBP-4.attlocal.net>
2025-03-25 22:41:12 -0700

    Solve second time

---
+++ 206_Reverse_Linked_list.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+
+    while (cur != NULL) {
+        struct ListNode* tmp = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = tmp;
+    }
+
+    return prev;
+}
