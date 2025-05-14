90918e340069231f651b88b95fc6455a2e22956c
Oreno <kanekosho@Kanekos-MBP-4.attlocal.net>
2025-05-14 08:48:58 -0700

    new file: 338_Counting Bits.c : first approach

---
+++ 338_Counting Bits.c
@@ -0,0 +1,63 @@
+/*
+Easy
+Topics
+Companies
+Hint
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+
+0 <= n <= 105
+ 
+
+Follow up:
+
+It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+*/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* countBits(int n, int* returnSize) {
+    if(n <= 0 || !returnSize){
+        return NULL;
+    }
+
+    int *ret = calloc(n+1, sizeof(int));
+    *returnSize = n+1;
+    for(int i = 0; i < n+1; i++){
+        int count = 0;
+        int val = i;
+        while(val > 0){
+            count++;
+            val &= (val - 1);
+        }
+        ret[i] = count;
+    }
+     
+    return ret; 
+}
